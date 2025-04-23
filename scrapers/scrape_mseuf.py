b83483258b4e4e84ebafd9b661ec21c06fe13ec1
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-04-23 22:20:10 +0800

    feat(sc-mseuf): implement base scraping logic

--- scrapers/scrape_mseuf.py
+++ scrapers/scrape_mseuf.py
@@ -1,8 +1,40 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+import time
+import random
 
 def main():
-    pass
+    # Data storage
+    program_df = pd.DataFrame(columns=['id', 'program_name', 'major', 'degree_type', 'department'])
+    program_campus_df = pd.DataFrame(columns=['id', 'program_id', 'campus'])
+    program_peo_df = pd.DataFrame(columns=['id', 'program_id', 'peo'])
+    program_outcomes_df = pd.DataFrame(columns=['id', 'program_id', 'outcome'])
+
+    url_response = requests.get('https://mseuf.edu.ph/programs')
+    soup = BeautifulSoup(url_response.text, 'lxml')
+
+    # Field data extraction
+    cards = soup.find_all('div', class_='card-panel outlined program-card waves-effect')
+    for card in cards:
+        program_name = card.find('p', class_='program')
+        degree_type = card.find('p', class_='degree')
+        major = card.find('p', class_='majors')
+        campus = card.find('p', class_='campus')
+
+        # Text extraction (error handled)
+        program_name_txt = program_name.get_text(strip=True) if program_name else 'N/A'
+        degree_type_txt = degree_type.get_text(strip=True) if degree_type else 'N/A'
+        major_txt = major.get_text(strip=True) if major else 'N/A'
+        campus_txt = campus.get_text(strip=True) if campus else 'N/A'
+
+        print(f'Program Name: {program_name_txt}')
+        print(f'Degree Type: {degree_type_txt}')
+        print(f'Majors: {major_txt}')
+        print(f'Campus: {campus_txt}')
+        print()
+
+        time.sleep(random.uniform(1, 3))
 
 if __name__ == '__main__':
     main()
